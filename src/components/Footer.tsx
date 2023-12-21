08ac0a28df8f078d38de32860bea2319c72d83c0
Chris Weiskopf <chris@chriscancode.io>
2023-12-21 08:22:37 -0500

    Started working on the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,25 @@
+import "../styles/Footer.css";
+
 function Footer() {
   return (
-    <footer>
-      <div>TODO: Work on footer</div>
-    </footer>
+    <section className="footer-container">
+      <ul>
+        <li>
+          <a href="https://github.com/Sagelyyy" target="_blank">
+            Github
+          </a>
+        </li>
+        <li>
+          <a href="https://www.linkedin.com/in/chriscancode/" target="_blank">
+            LinkedIn
+          </a>
+        </li>
+        <li>
+          <a href="mailto:chris@chriscancode.io">Contact Me</a>
+        </li>
+        <li>© 2023 Christian Weiskopf</li>
+      </ul>
+    </section>
   );
 }
 
