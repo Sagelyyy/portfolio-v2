488a49d5b7028bf500028233f30ded9e5b09c221
Chris Weiskopf <chris@chriscancode.io>
2024-02-19 10:52:44 -0500

    Prism has been added for better looking code blocks!

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,7 +1,12 @@
 import { postInterface } from "../utils/interfaces";
 import { DateTime } from "luxon";
 import dompurify from "dompurify";
+import { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/themes/prism.css";
+import "prismjs/themes/prism-twilight.css";
 import "../styles/Post.css";
+
 // import { useContext } from "react";
 // import { UserContext } from "../App";
 
@@ -12,6 +17,10 @@ function Post({ id, title, content, user, created_at }: postInterface) {
   const formattedDate =
     DateTime.fromISO(created_at).toFormat("MM/dd/yyyy hh:mm a");
 
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
+
   return (
     <div className="post-container" key={id}>
       <h3 className="post-title">{title}</h3>
@@ -19,7 +28,7 @@ function Post({ id, title, content, user, created_at }: postInterface) {
         Published by <span className="post-user">{user}</span> at{" "}
         {formattedDate}
       </p>
-      <p
+      <div
         className="post-content"
         dangerouslySetInnerHTML={{
           __html: dompurify.sanitize(content, { FORCE_BODY: true }),
